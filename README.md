ac269ecf474396f0382869398cbbc5e37f04bdd9
jaxdios <jay.tauron@gmail.com>
2014-12-18 14:30:36 +0000

    Changed to Handlebars for html templating and compacted JS

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Quiz
 JavaScript based quiz game
 --------------------------
 
-Just a simple quiz game I'm working using jQuery UI, Mustache and JSON.
+Just a simple quiz game I'm working using jQuery UI, Handlebars and JSON.
 
 Any suggestions and ideas may be useful!
--- js/script.js
+++ js/script.js
@@ -1,196 +1,183 @@
-$(document).ready(function() {
+/*global Handlebars */
+$(document).ready(function () {
+    // Global vars
+    var looper = 0; // looper counter keeps track of current question
+    var answers = [];
+    var questions;
+    var transTime = 1e3; //Time in ms for wrapper transition time
+    var H = Handlebars;
 
-	function quizLoop() {
+    function quizLoop() {
 
-		// Show end screen if questions finished
-		if (looper == questions.length) {
-			displayEnd();
-			return;
-		}
-		var tempObj = questions[looper];
-		var tempAnswer = tempObj.correctAnswer;
+        // Show end screen if questions finished
+        if (looper >= questions.length) {
+            displayEnd();
+            return;
+        }
+        var tempObj = questions[looper];
 
-		// Display question number and question
-		$('#question').html('<span class="number">Q'+(looper+1)+'. </span>'+tempObj.question);
+        // Display question number and question
+        $('#question').html('<span class="number">Q' + (looper + 1) + '. </span>' + tempObj.question);
 
-		// Create options
-		for (var j = 0; j < 3; j++) {
-			var info = {
-				num: j,
-				choice: tempObj.choices[j]
-			}
+        // Create options
+        var templ_choice = H.compile($('#templ-choice').html());
+        $('#choices').append(templ_choice(tempObj.choices));
 
-			var templ_choice = $('#templ-choice').html();
-			Mustache.parse(templ_choice);
-			var element = Mustache.render(templ_choice, info);
-			$('#choices').append(element);
-		}
+        // Make option highlighting method
+        var previd;
+        var id;
+        $('input[type=radio][name=option]').change(function () {
+            id = $('input:radio:checked').attr('id');
+            if (previd !== undefined) {
+                $('label[for=' + previd + '] li').toggleClass('selected');
+            }
+            $('label[for=' + id + '] li').toggleClass('selected');
+            previd = id;
+        });
 
-		// Make option highlighting method
-		var previd;
-		var id;
-		$('input[type=radio][name=option]').change(function() {
-			id = $('input:radio:checked').attr('id');
-		    if (previd !== undefined) {
-		       	$('label[for='+previd+'] li').toggleClass('selected');
-		    }
-		    $('label[for='+id+'] li').toggleClass('selected');
-		    previd = id;
-		});
+        // Check if question already has answer and highlight it
+        if (answers[looper]) {
+            $('label[for=' + answers[looper] + '] li').toggleClass('selected');
+            $('input:radio[id=' + answers[looper] + ']').each(function () {
+                this.checked = true;
+            });
+            previd = answers[looper];
+        }
 
-		// Check if question already has answer and highlight it
-		if(answers[looper]) {
-			$('label[for='+answers[looper]+'] li').toggleClass('selected');
-			$('input:radio[id='+answers[looper]+']').each(function(){
-				this.checked = true;
-			});
-			previd = answers[looper];
-		}
+        // bring wrapper down
+        $('#wrapper').show("blind", transTime, function () {
+            $('.button').show("fast");
+        });
+    }
 
-		// bring wrapper down
-		$('#wrapper').show("blind", transTime, function(){
-			$('.button').show("fast");
-		});
-	}
 
-	function addAns(){
-		// Check an answer is chosen
-		if ( !$("input:radio:checked").length ) {
-			alert("Select an answer!");
-		} else {
-			answers[looper ] = $("input:radio:checked").attr("value");
-			$('#wrapper').hide("blind", transTime, function(){
-				// Remove current question from html
-				$('#question').html('');
-				$('#choices').html('');
-				// Increment loop counter and ask next question
-				looper++;
-				quizLoop();
-			});
-		}
-	}
+    function displayEnd() {
+        var score = 0;
 
-	function goBack(){
-		// If user is at beginning, do nothing
-		if (looper === 0) return;
-		// Remove current question from html
-		$('#wrapper').hide("blind", transTime, function(){
-			$('#question').html('');
-			$('#choices').html('');
-			// Deccrement loop counter and ask previous question
-			looper--;
-			quizLoop();
-		});
-	}
+        $("#next_button").hide();
+        $('#question_wrap').hide();
+        $('#title').text('Results');
+        $('#wrapper').append('<div id="results"></div>');
 
-	function displayEnd() {
-		var score = 0;
-		$("#next_button").hide();
-		$('#question_wrap').hide();
-		$('#title').html('Results');
-		$('#wrapper').append('<div id="results"></div>');
+        // Compare answers to correct answers
+        var i;
+        var correct;
+        var templ_result = H.compile($('#templ-result').html());
+        for (i = 0; i < questions.length; i++) {
+            correct = false;
+            if (answers[i] === questions[i].correctAnswer) {
+                correct = true;
+                score++;
+            }
 
-		// Compare answers to correct answers
-		for (var i = 0; i < questions.length; i++) {
-			var msg;
-			var colour;
+            // Mustache rendering to put in result
+            $('#results').append(templ_result({
+                message: correct ? "Correct" : "Incorrect",
+                question_num: i + 1,
+                colour: correct ? "green" : "red",
+                correct: correct,
+                question: questions[i].question
+            }));
+        }
 
-			if (answers[i] == questions[i].correctAnswer) {
-				correct = true;
-				colour = "green";
-				score++;
-			} else {
-				correct = false;
-				colour = "red";
-			}
+        // Display final score and rounded percentage
+        var percentage = Math.round((score / questions.length) * 100);
 
-			var info = {
-				message: correct ? "Correct" : "Incorrect",
-				question_num: i+1,
-				colour: colour,
-				correct: correct,
-				question: questions[i].question
-			}
+        // more Mustache to put in score and restart button
+        var templ_score = H.compile($('#templ-score').html());
+        $('#wrapper').append(templ_score({
+            score: score,
+            no_qs: questions.length,
+            percentage: percentage
+        }));
 
-			// mustache rendering to put in result
-			var templ_result = $('#templ-result').html();
-			Mustache.parse(templ_result);
-			var result = Mustache.render(templ_result, info);
-			$('#results').append(result);
-			// todo: SHOW ANSWER
-		}
+        //Hide results on creation to later transition in
+        $('.result').hide();
+        $('.score').hide();
 
-		// Display final score and rounded percentage
-		var percentage = Math.round((score/questions.length)*100);
+        $('#wrapper').show("blind", transTime, function () {
+            $('.score').show("fade");
+            $("#results div").first().show("fast", function showNext() {
+                $(this).next("div").show("fast", showNext);
+            });
+        });
 
-		var info = {
-			score: score,
-			no_qs: questions.length,
-			percentage: percentage
-		}
-		// more mustache to put in score and restart button
-		var templ_score = $('#templ-score').html();
-		Mustache.parse(templ_score);
-		var x = Mustache.render(templ_score, info);
-		$('#wrapper').append(x);
+        // Add event listener to restart button
+        $('#restart').click(function () {
+            $('#wrapper').hide("blind", transTime, function () {
+                looper = 0;
+                answers = [];
+                $('#title').text('QUIZ');
+                $('#question_wrap').show();
+                $('.score').remove();
+                $('#results').remove();
+                $('#restart').remove();
+                $('#question').html('');
+                $('#choices').html('');
+                quizLoop();
+            });
+        });
+    }
 
-		//Hide results on creation then transition them in
-		$('.result').hide();
-		$('.score').hide();
+    function addAns() {
+        // Check an answer is chosen
+        if (!$("input:radio:checked").length) {
+            window.alert("Select an answer!");
+        } else {
+            answers[looper] = parseInt($("input:radio:checked").attr("value"), 10);
+            $('#wrapper').hide("blind", transTime, function () {
+                // Remove current question from html
+                $('#question').html('');
+                $('#choices').html('');
+                // Increment loop counter and ask next question
+                looper++;
+                quizLoop();
+            });
+        }
+        console.log(answers);
+    }
 
-		$('#wrapper').show("blind", transTime, function(){
-			$('.score').show("fade");
-			$("#results div").first().show("fast", function showNext(){
-					$(this).next("div").show("fast", showNext);
-			});
-		});
+    function goBack() {
+        // If user is at beginning, do nothing
+        if (looper === 0) {
+            return;
+        }
+        // Remove current question from html
+        $('#wrapper').hide("blind", transTime, function () {
+            $('#question').html('');
+            $('#choices').html('');
+            // Deccrement loop counter and ask previous question
+            looper--;
+            quizLoop();
+        });
+    }
 
-		// Add event listener to restart button
-		$('#restart').click(function(){
-			$('#wrapper').hide("blind", transTime, function(){
-				looper = 0;
-				answers = [];
-				$('#title').html('QUIZ');
-				$('#question_wrap').show();
-				$('.score').remove();
-				$('#results').remove();
-				$('#restart').remove();
-				$('#question').html('');
-				$('#choices').html('');
-				quizLoop();
-			});
-		});
-	}
+    $('#back_button').click(goBack);
+    $('#next_button').click(addAns);
 
-	// Global vars
-	var looper = 0; // looper counter keeps track of current question
-	var answers = [];
-	var questions = [];
-	var transTime = 1000; //Time in ms for wrapper transition time
+    // Request questions from JSON file
+    $.getJSON("js/questions.json", function (data) {
+        questions = data.questions;
+    }).done(function () {
+        quizLoop();
+    }).fail(function () {
+        window.alert('Sorry, there was a problem!');
+    });
 
-	$('#back_button').click(goBack);
-	$('#next_button').click(addAns);
-
-	// Request questions from JSON file
-	$.getJSON("js/questions.json", function(data) {
-		$.each(data.questions, function(key, val) {
-			questions.push(val);
-		});
-	}).done(function(){
-		quizLoop();
-	}).fail(function(){
-		alert('Sorry, there was a problem!');
-	});
-
-/******** DEV ONLY *********/
-	function testEndScreen(mode) {
-		if (mode == 'rand') answers = [0,0,0,0,0,0,0];
-		else if (mode == 'correct') answers = [1,0,0,1,2,0,0];
-		else if (mode == 'wrong') answers = [0,1,1,0,0,1,1];
-		else throw new Error('Invalid EndScreen test mode');
-		looper = 7;
-	}
-	//testEndScreen('wrong');
-/**************************/
+    /******** DEV ONLY *********/
+    function testEndScreen(mode) {
+        if (mode === 'rand') {
+            answers = [0, 0, 0, 0, 0, 0, 0];
+        } else if (mode === 'correct') {
+            answers = [1, 0, 0, 1, 2, 0, 0];
+        } else if (mode === 'wrong') {
+            answers = [0, 1, 1, 0, 0, 1, 1];
+        } else {
+            console.error('Invalid EndScreen test mode');
+        }
+        looper = 7;
+    };
+    //testEndScreen('rand');
+    /**************************/
 
 });
