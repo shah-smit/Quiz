331791d6af19c3f667a098ae4bd1291b39549dec
jaxdios <jay.tauron@gmail.com>
2014-12-22 19:22:07 +0000

    static sidebar

--- js/script.js
+++ js/script.js
@@ -1,24 +1,67 @@
 $(function () {
-    var looper = 0; // looper counter keeps track of current question
-    var answers = [];
-    var questions;
-    var transTime = 300; //Time in ms for wrapper transition time
-    var H = Handlebars;
-    var ls = localStorage;
+    var looper = 0, // looper counter keeps track of current question
+        answers = [],
+        questions,
+        transTime = 300, //Time in ms for wrapper transition time
+        H = Handlebars,
+        ls = localStorage,
 
     //Cache DOM
-    $wrapper = $('#wrapper');
+        $wrapper = $('#wrapper'),
 
-    $Qwrap = $wrapper.find('#question_wrap');
-    $question = $Qwrap.find('#question');
-    $back = $Qwrap.find('#back_button');
-    $next = $Qwrap.find('#next_button');
-    $fb = $back.add($next);
+        $Qwrap = $wrapper.find('#question_wrap'),
+        $question = $Qwrap.find('#question'),
+        $back = $Qwrap.find('#back_button'),
+        $next = $Qwrap.find('#next_button'),
+        $fb = $back.add($next),
 
-    $results = $wrapper.find('#results');
-    $score = $wrapper.find('.score');
-    $restart = $wrapper.find('#restart');
-    $title = $wrapper.find('#title');
+        $results = $wrapper.find('#results'),
+        $score = $wrapper.find('.score'),
+        $restart = $wrapper.find('#restart'),
+        $title = $wrapper.find('#title');
+
+    function initUser() {
+        if (!Modernizr.localstorage) return;
+
+        $stats = $('.stats'),
+        $create = $('.create'),
+        $main = $('#side .main');
+
+        function insStats() {
+            $stats.find('.username').html(ls.username);
+            $stats.find('.games .number').html(ls.games);
+            $stats.find('.hscore .number').html(ls.hscore);
+            $main.html('Profile');
+        }
+
+        function submitUser() {
+            var un = $(this).parent().find('input').val();
+            if (!un) {
+                alert('Enter a username!');
+                return;
+            }
+            ls.username = un;
+            ls.games = 0;
+            ls.hscore = 0;
+
+            insStats();
+
+            $create.toggle('fast');
+            $stats.toggle('fast');
+        }
+
+        if (ls.length) {
+            insStats();
+            $stats.toggle("fast")
+        } else {
+            $main.html('Create Profile')
+            $create.toggle();
+        }
+
+        $('.go').click(submitUser);
+
+    };
+    initUser();
 
     function message(m) {
         // slide in message box
@@ -120,17 +163,6 @@ $(function () {
         // Add these numbers to DOM
         $('.games .number').html(ls.games);
         $('.hscore .number').html(ls.hscore);
-
-        // Add event listener to restart button
-        $restart.click(function () {
-            looper = 0;
-            answers = [];
-            $results.html('');
-            $score.html('');
-            $fb.show();
-            $title.text('QUIZ');
-            quizLoop();
-        });
     }
 
     function toggleSelected(id){
@@ -159,8 +191,19 @@ $(function () {
         looper && swapQ(-1);
     }
 
+    function restart() {
+        looper = 0;
+        answers = [];
+        $results.html('');
+        $score.html('');
+        $fb.show();
+        $title.text('QUIZ');
+        quizLoop();
+    }
+
     $back.click(goBack);
     $next.click(addAns);
+    $restart.click(restart);
 
     // Request questions from JSON file
     $.getJSON("js/questions.json", function (data) {
